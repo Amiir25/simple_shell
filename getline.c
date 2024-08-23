3d4dd14c83b4cb7e3216359f1536a19990257da7
Amiir25 <amirsadik636@gmail.com>
2024-08-23 20:31:46 +0300

    Update getline.c

--- getline.c
+++ getline.c
@@ -8,42 +8,47 @@
  *
  * Return: Number of characters read, or -1 on failure
  */
+
 ssize_t getline(char **lineptr, size_t *n, FILE *stream)
 {
-    static char buffer[1024];
-    static size_t pos, length;
-    ssize_t num_read = 0;
-    char *newline;
+	static char buffer[1024];
+	static size_t pos, length;
+	ssize_t num_read;
+	char *newline;
 
-    if (*lineptr == NULL || *n == 0) {
-        *n = 1024;
-        *lineptr = malloc(*n);
-        if (*lineptr == NULL)
-            return (-1);
-    }
+	num_read = 0;
+	if (*lineptr == NULL || *n == 0)
+	{
+		*n = 1024;
+		*lineptr = malloc(*n);
+		if (*lineptr == NULL)
+			return (-1);
+	}
 
-    while (1) {
-        if (pos >= length) {
-            pos = 0;
-            length = read(fileno(stream), buffer, sizeof(buffer));
-            if (length <= 0)
-                return (-1);
-        }
+	while (1)
+	{
+		if (pos >= length)
+		{
+			pos = 0;
+			length = read(fileno(stream), buffer, sizeof(buffer));
+			if (length <= 0)
+				return (-1);
+		}
+		(*lineptr)[num_read++] = buffer[pos++];
 
-        (*lineptr)[num_read++] = buffer[pos++];
-
-        if (num_read >= *n) {
-            *n += 1024;
-            *lineptr = realloc(*lineptr, *n);
-            if (*lineptr == NULL)
-                return (-1);
-        }
-
-        if ((newline = strchr(*lineptr, '\n'))) {
-            *newline = '\0';
-            break;
-        }
-    }
-
-    return (num_read);
+		if (num_read >= *n)
+		{
+			*n += 1024;
+			*lineptr = realloc(*lineptr, *n);
+			if (*lineptr == NULL)
+				return (-1);
+		}
+		newline = strchr(*lineptr, '\n');
+		if (newline != NULL)
+		{
+			*newline = '\0';
+			break;
+		}
+	}
+	return (num_read);
 }

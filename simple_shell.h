030eba135950b9192610de7f31d571930952b5a0
Amiir25 <144411155+Amiir25@users.noreply.github.com>
2024-08-23 04:57:35 -0700

    Create simple_shell.h

---
+++ simple_shell.h
@@ -0,0 +1,40 @@
+#ifndef SIMPLE_SHELL_H
+#define SIMPLE_SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+
+extern char **environ;
+
+#define MAX_ARGS 64
+#define DELIM " \t\r\n\a"
+
+// Function Prototypes
+char *read_command(void);
+char **parse_command(char *line);
+int execute_command(char **args);
+void execute_file(const char *filename);
+
+// Built-in commands
+int shell_exit(char **args);
+int shell_env(char **args);
+int shell_cd(char **args);
+int shell_setenv(char **args);
+int shell_unsetenv(char **args);
+int shell_alias(char **args);
+
+// Helper functions
+int _strcmp(const char *s1, const char *s2);
+int _strlen(const char *str);
+char *_strdup(const char *str);
+void handle_comments(char *line);
+char *search_path(char *command);
+
+#endif

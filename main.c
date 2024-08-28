0123827da1298fef4714610c6afc69b7e4bb0245
Amiir25 <144411155+Amiir25@users.noreply.github.com>
2024-08-28 05:24:11 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,28 @@
 #include "simple_shell.h"
 
 /**
- * main - Entry point for the shell program.
+ * main - Entry point for the shell.
  * @argc: Argument count.
  * @argv: Argument vector.
  * 
- * Return: Always 0 on success.
+ * Return: Exit status of the shell.
  */
 int main(int argc, char **argv)
 {
-    (void)argc;
-    (void)argv;
-    shell_loop(argv[0]);
+    if (argc == 2)
+    {
+        execute_from_file(argv[1], argv[0]);
+    }
+    else if (argc == 1)
+    {
+        shell_loop(argv[0]);
+    }
+    else
+    {
+        fprintf(stderr, "%s: Usage: %s [filename]\n", argv[0], argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
     return (0);
 }
+

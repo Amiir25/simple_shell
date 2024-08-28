98709b3694660338d908acc985fa2a8dd9f14b22
Amiir25 <144411155+Amiir25@users.noreply.github.com>
2024-08-28 06:15:01 -0700

    Create path.c

---
+++ path.c
@@ -0,0 +1,33 @@
+#include "simple_shell.h"
+
+/**
+ * find_command - Finds the full path of a command in the PATH.
+ * @command: The command to find.
+ *
+ * Return: The full path of the command, or NULL if not found.
+ */
+
+char *find_command(char *command)
+{
+    char *path = getenv("PATH");
+    char *token;
+    char full_path[1024];
+
+    if (command[0] == '/' || command[0] == '.')
+    {
+        return (strdup(command));
+    }
+
+    token = strtok(path, ":");
+    while (token != NULL)
+    {
+        snprintf(full_path, sizeof(full_path), "%s/%s", token, command);
+        if (access(full_path, X_OK) == 0)
+        {
+            return (strdup(full_path));
+        }
+        token = strtok(NULL, ":");
+    }
+
+    return (NULL);
+}
